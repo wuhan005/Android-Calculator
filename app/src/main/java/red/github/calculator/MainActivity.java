3aed387d61191ef136c7803963c4dfb3abe6928d
John Wu <524306184@qq.com>
2019-04-30 16:11:56 +0800

    controll button finished

--- app/src/main/java/red/github/calculator/MainActivity.java
+++ app/src/main/java/red/github/calculator/MainActivity.java
@@ -8,12 +8,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView resultBox;
+    private TextView resultBox, controlBox;
     private Number calResult = 0;
+    private String nowControl = "";
 
-    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    private Number num1, num2;
+
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, plus, minus, multiply, devide, equal;
     private Button[] numButtons = new Button[] {btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9};
     private Integer[] buttonID = new Integer[] {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9};
+
+    private Button[] controlButtons = new Button[] {plus, minus, multiply, devide, equal};
+    private Integer[] controlID = new Integer[] {R.id.plus, R.id.minus, R.id.multiply, R.id.devide, R.id.equal};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         this.resultBox = findViewById(R.id.ResultBox);
         this.resultBox.setText(this.calResult.toString());
 
+        this.controlBox = findViewById(R.id.ControlBox);
+
         this.InitUI();
-        System.out.println("okokok");
     }
 
     private void InitUI(){
@@ -37,6 +44,41 @@ public class MainActivity extends AppCompatActivity {
                     NumButtonClicked(v.getId());
                 }
             });
+        }
+
+        // set the control buttons.
+       for(int i = 0; i < 5; i++){
+           this.controlButtons[i] = findViewById(this.controlID[i]);
+           this.controlButtons[i].setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                    ControlButtonClicked(v.getId());
+               }
+           });
+       }
+
+    }
+
+    private void ControlButtonClicked(Integer id){
+        switch (id){
+            case R.id.plus:
+                this.nowControl = "plus";
+                this.controlBox.setText("+");
+                break;
+            case R.id.minus:
+                this.nowControl = "minus";
+                this.controlBox.setText("-");
+                break;
+            case R.id.multiply:
+                this.nowControl = "multiply";
+                this.controlBox.setText("×");
+                break;
+            case R.id.devide:
+                this.nowControl = "devide";
+                this.controlBox.setText("÷");
+                break;
+            case R.id.equal:
+                break;
         }
     }
 
